e657e05c02c88d747b63a2ea98cdfb7ed6b62db5
sepiggy <jms1209@qq.com>
2022-12-10 23:56:10 +0800

    b4 p018 ok

--- src/test/java/cn/itcast/nio/c3/TestFileChannelTransferTo.java
+++ src/test/java/cn/itcast/nio/c3/TestFileChannelTransferTo.java
@@ -6,24 +6,29 @@ import java.io.IOException;
 import java.nio.channels.FileChannel;
 
 /**
- * FileChannel#transferTo 方法演示
+ * 两个FileChannel之间传输数据
+ * <p></p>
+ * FileChannel#transferTo方法演示
  */
 public class TestFileChannelTransferTo {
+
     public static void main(String[] args) {
-        try (
-                FileChannel from = new FileInputStream("data.txt").getChannel();
-                FileChannel to = new FileOutputStream("to.txt").getChannel();
-        ) {
+        // from只可以读数据
+        // to只可以写数据
+        try (FileChannel from = new FileInputStream("data.txt").getChannel();
+             FileChannel to = new FileOutputStream("to.txt").getChannel()) {
+
             long size = from.size();
-            // left 变量代表还剩余多少字节
+            // left变量代表还剩余多少字节
             for (long left = size; left > 0; ) {
-                System.out.println("position:" + (size - left) + " left:" + left);
-                // 效率高，底层会利用操作系统的零拷贝进行优化, 每次最多传输2g数据
-                // 采用多次传输的方法可以解决上述问题
+                System.out.println("position:" + (size - left) + ", left:" + left);
+                // 效率高，底层会利用操作系统的零拷贝进行优化,
+                // 坑: 每次最多传输2g数据! (采用多次传输的方法可以解决上述问题)
                 left -= from.transferTo((size - left), left, to);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
