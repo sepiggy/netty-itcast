30dc02472547da83bf11db260a41e16b7fc79d4e
sepiggy <jms1209@qq.com>
2022-12-13 11:54:23 +0800

    b4 p058 ok

--- src/test/java/cn/itcast/netty/c2/HelloClient.java
+++ src/test/java/cn/itcast/netty/c2/HelloClient.java
@@ -9,28 +9,41 @@ import io.netty.handler.codec.string.StringEncoder;
 import java.net.InetSocketAddress;
 
 /**
- * Netty客户端类
+ * HelloWorld客户端
+ * <br>
+ * 开发一个简单的服务器端和客戶端:
+ * <br>
+ * 1) 客戶端向服务器端发送 hello, world
+ * <br>
+ * 2) 服务器仅接收，不返回
  */
 public class HelloClient {
+
     public static void main(String[] args) throws InterruptedException {
-        // 1. Bootstrap: 客户端启动器
+        // 1. Bootstrap: 客户端启动器，负责组装netty组件，启动客户端
         new Bootstrap()
-            // 2. 添加 EventLoop
-            .group(new NioEventLoopGroup())
-            // 3. 选择客户端的 SocketChannel 实现 (对原生的 SocketChannel 进行封装)
-            .channel(NioSocketChannel.class)
-            // 4. 添加处理器
-            .handler(new ChannelInitializer<NioSocketChannel>() {
-                @Override // 在连接建立后被调用
-                protected void initChannel(NioSocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new StringEncoder());
-                }
-            })
-            // 5. 连接到服务器
-            .connect(new InetSocketAddress("localhost", 8080))
-            .sync() // 阻塞方法，直到连接建立
-            .channel() // 代表连接对象 SocketChannel
-            // 6. 向服务器发送数据
-            .writeAndFlush("hello, world"); // 发送数据
+                // 2. 添加EventLoop
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端的SocketChannel实现(对原生的SocketChannel进行封装)
+                .channel(NioSocketChannel.class)
+                // 4. 添加读写事件发生时的事件处理器（Handler）
+                .handler(
+                        // 5. 添加"Channel初始化器"
+                        //    注意：ChannelInitializer(Channel初始化器)是一种对Channel进行初始化的特殊的事件处理器(Handler)
+                        //    注意：ChannelInitializer会在连接建立后(即accept事件发生后)执行initChannel方法
+                        new ChannelInitializer<NioSocketChannel>() {
+                            @Override // 在连接建立后被调用
+                            protected void initChannel(NioSocketChannel ch) throws Exception {
+                                // 注意：在Netty中收发数据都会走Handler
+                                ch.pipeline().addLast(new StringEncoder()); // 编码器: String->ByteBuf
+                            }
+                        })
+                // 6. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync() // 阻塞方法，直到连接建立
+                .channel() // 代表服务器和客户端通信的Channel
+                // 7. 向服务器端发送数据
+                .writeAndFlush("hello, world"); // 向Channel写数据(发送数据到服务器端)
     }
+
 }
